4446a05724a92d2ec58145b91377f09cba3b3863
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-09-23 13:42:17 -0400

    7 Segment

---
+++ 7seg.v
@@ -0,0 +1,9 @@
+module hello(input c1, input c0, output [0:6] h);
+    assign h[0] = ~c1 & c0;
+    assign h[1] = c0;
+    assign h[2] = ~c1 | c0;
+    assign h[3] = c1 ~^ c0;
+    assign h[4] = ~c1 | ~c0;
+    assign h[5] = h[4];
+    assign h[6] = ~c1 & c0;
+endmodule
